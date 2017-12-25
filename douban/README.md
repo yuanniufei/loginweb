aa26d922bd9714e39b9b13bf86793acf0765bfa1
yuanniufei <yuanniufei@vipkid.com.cn>
2017-12-25 10:29:00 +0800

    add douban top250 films info

---
+++ douban/README.md
@@ -0,0 +1,16 @@
+# 爬取豆瓣top250高分电影保存到文本
+
+## HOW TO RUN (python3 & python2)
+
+安装依赖
+
+```shell
+pip install -r requirements.txt
+```
+
+运行
+
+```shell
+python douban/douban_top250_movies.py
+```
+
---
+++ douban/douban_top250_movies.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+base_url = 'https://movie.douban.com/top250'
+headers = {
+    'Host': 'movie.douban.com',
+    'User_Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+}
+
+session = requests.session()
+
+
+def my_print(movie_list):
+    columns = ['电影排名: ', '电影名称: ', '电影评分: ', '评分人数: ', '电影简评: ']
+    for movie in movie_list:
+        for k, v in enumerate(movie):
+            print(columns[k], v)
+        print('*' * 50)
+
+
+def save_as_text(movie_list):
+    columns = ['电影排名: ', '电影名称: ', '电影评分: ', '评分人数: ', '电影简评: ']
+    with open('douban_top250_movies.txt', 'w') as f:
+        for movie in movie_list:
+            for k, v in enumerate(movie):
+                f.write(columns[k] + v + '\n')
+            f.write('*' * 50 + '\n')
+
+
+def get_movie_info(session, url):
+    try:
+        resp = session.get(url, headers=headers)
+    except Exception as e:
+        print('request exception', e)
+
+    soup = BeautifulSoup(resp.content, 'lxml')
+    return soup
+
+
+def parser_soup(soup):
+    info_list = []
+    movie_list = soup.find(class_='grid_view').find_all('li')
+    for movie in movie_list:
+        rank = movie.find('em').string
+        name = movie.find(class_='info').find(class_='title').string
+        score = movie.find('span', 'rating_num').string
+        vote_num = movie.find(class_='bd').find_all('span')[-2].string
+        vote_num = re.match(r'[0-9]+', vote_num).group(0)
+
+        comment = ''
+        if movie.find(class_='inq'):
+            comment = movie.find('span', 'inq').string
+        info_list.append((rank, name, score, vote_num, comment))
+    return info_list
+
+
+if __name__ == '__main__':
+    movie = []
+    for i in range(10):
+        url = base_url + '?start={}&filter='.format(i * 25)
+        soup = get_movie_info(session, url)
+        movie.extend(parser_soup(soup))
+    my_print(movie)
+    save_as_text(movie)
